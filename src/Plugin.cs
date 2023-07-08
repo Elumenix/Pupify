1f1a20d216220ac1583e3687590d6ea852e267ad
Elumenix <89927445+Elumenix@users.noreply.github.com>
2023-07-07 21:34:32 -0400

    Made repository programming ready

---
+++ src/Plugin.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Security;
+using System.Security.Permissions;
+using BepInEx;
+
+#pragma warning disable CS0618
+
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+namespace Pupify;
+
+[BepInPlugin("elumenix.pupify", "Pupify", "1.0.0")]
+public class Plugin : BaseUnityPlugin
+{
+    private void OnEnable()
+    {
+        On.RainWorld.OnModsInit += RainWorld_OnModsInit;
+    }
+
+    private bool IsInit;
+    private void RainWorld_OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    {
+        orig(self);
+        try
+        {
+            if (IsInit) return;
+
+            //Your hooks go here
+
+            On.RainWorldGame.ShutDownProcess += RainWorldGame_ShutDownProcess;
+            On.GameSession.ctor += GameSession_ctor;
+            
+            IsInit = true;
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex);
+            throw;
+        }
+    }
+    
+    private void RainWorldGame_ShutDownProcess(On.RainWorldGame.orig_ShutDownProcess orig, RainWorldGame self)
+    {
+        orig(self);
+        ClearMemory();
+    }
+    private void GameSession_ctor(On.GameSession.orig_ctor orig, GameSession self, RainWorldGame game)
+    {
+        orig(self, game);
+        ClearMemory();
+    }
+
+    #region Helper Methods
+
+    private void ClearMemory()
+    {
+        //If you have any collections (lists, dictionaries, etc.)
+        //Clear them here to prevent a memory leak
+        //YourList.Clear();
+    }
+
+    #endregion
+}
